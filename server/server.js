4ede748600f289cc611490221bdf43e799685478
Punith <punith.ch@gmail.com>
2018-03-03 15:14:33 -0700

    init commit

--- server/server.js
+++ server/server.js
@@ -1,9 +1,5 @@
-const express = require('express');
-const http = require('http');
-
 const path = require('path');
-
-
+const express = require('express');
 
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
@@ -11,5 +7,5 @@ var app= express();
 
 app.use(express.static(publicPath));
 app.listen(port, ()=>{
-console.log('Server is up on port `$(port)`);
-})
+console.log(`Server is up on port ${port}`);
+});
